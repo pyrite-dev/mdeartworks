3b19e37567422d2a20afd8442fc4206a5b83b763
NishiOwO <nishi@nishi.boats>
2025-12-27 19:49:34 +0900

    yes

--- generate_png.sh
+++ generate_png.sh
@@ -4,6 +4,7 @@ for i in 128 64 32 16; do
 	echo "-- ${i}x${i}"
 	for j in actions apps categories devices emblems emotes mimetypes places status; do
 		mkdir -p ../${i}x${i}/${j}
+		echo $j > ../${i}x${i}/${j}/README.txt
 	done
 	find . -name "*.svg" | while read a; do
 		echo "$a"
