b655b990524cfbf5ade26ccb478c77b2ce17c26c
NishiOwO <nishi@nishi.boats>
2025-12-27 19:58:02 +0900

    add mde logo back

--- generate_png.sh
+++ generate_png.sh
@@ -6,6 +6,9 @@ for i in 128 64 32 16; do
 		mkdir -p ../${i}x${i}/${j}
 		echo $j > ../${i}x${i}/${j}/README.txt
 	done
+	if [ ! "$i" = "128" ]; then
+		convert ../128x128/apps/mde.png -scale x${i} ../${i}x${i}/apps/mde.png
+	fi
 	find . -name "*.svg" | while read a; do
 		echo "$a"
 		rsvg-convert --height=$i -o ../${i}x${i}/`echo $a | cut -d. -f2`.png $a
